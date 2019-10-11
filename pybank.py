868888c0247ff739c5db243b62168cd871ea84bb
pesheldon94 <54085509+pesheldon94@users.noreply.github.com>
2019-10-11 16:31:45 -0400

    Add files via upload

---
+++ pybank.py
@@ -0,0 +1,59 @@
+import os
+import csv
+bank_csv = os.path.join('budget_data.csv')
+total_months = 0
+total_pl = 0
+value = 0
+change = 0
+dates = []
+profits = []
+
+#Pull the data
+with open(bank_csv, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    #Reading the header row
+    csv_header = next(csvreader)
+
+    first_row = next(csvreader)
+    total_months += 1
+    total_pl += int(first_row[1])
+    value = int(first_row[1])
+    
+    for row in csvreader:
+    
+        dates.append(row[0])
+        
+        # Add change to list of changes
+        change = int(row[1])-value
+        profits.append(change)
+        value = int(row[1])
+        
+        #Total number of months
+        total_months += 1
+
+        #Total net amount of "Profit/Losses over entire period"
+        total_pl = total_pl + int(row[1])
+
+    #Greatest increase in profits
+    greatest_increase = max(profits)
+    greatest_index = profits.index(greatest_increase)
+    greatest_date = dates[greatest_index]
+
+    #Greatest decrease
+    greatest_decrease = min(profits)
+    worst_index = profits.index(greatest_decrease)
+    worst_date = dates[worst_index]
+
+    #Average change in "Profit/Losses between months over entire period"
+    avg_change = sum(profits)/len(profits)
+    
+
+#Displaying information
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {str(total_months)}")
+print(f"Total: ${str(total_pl)}")
+print(f"Average Change: ${str(round(avg_change,2))}")
+print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
