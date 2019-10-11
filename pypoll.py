5563ea571dd0cb628154929e79006340e4ad3a57
pesheldon94 <54085509+pesheldon94@users.noreply.github.com>
2019-10-11 16:28:24 -0400

    Add files via upload
    Poll Data CSV file would not pull in because too large

---
+++ pypoll.py
@@ -0,0 +1,51 @@
+import os 
+import csv
+#Pull Data in - file saved on local drive directly/in reporistory folder
+polling_csv = os.path.join('election_data.csv')
+#Create lists for results
+candidates = []
+num_votes = []
+percent_votes = []
+
+# Create counter to tally votes and identify unique values
+total_votes = 0
+with open(polling_csv, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        # Add to our vote-counter 
+        total_votes += 1 
+
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            num_votes.append(1)
+        else:
+            index = candidates.index(row[2])
+            num_votes[index] += 1
+    
+    # Tally percentages 
+    for votes in num_votes:
+        percentage = (votes/total_votes) * 100
+        percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
+        percent_votes.append(percentage)
+    
+    # Calculate Winner
+    winner = max(num_votes)
+    index = num_votes.index(winner)
+    winning_candidate = candidates[index]
+
+# Displaying results
+print("Election Results")
+print("--------------------------")
+print(f"Total Votes: {str(total_votes)}")
+print("--------------------------")
+for i in range(len(candidates)):
+    print(f"{candidates[i]}: {str(percent_votes[i])} ({str(num_votes[i])})")
+print("--------------------------")
+print(f"Winner: {winning_candidate}")
+print("--------------------------")
+
+
